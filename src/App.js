0333c9427fc8795d46fc335e75a30b44700bcf6e
alissanguyen <alissa.nguyen1211@gmail.com>
2020-09-07 22:05:50 -0700

    Version 1.0.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [userInput, setUserInput] = React.useState("");
+
+  const [fetchedUsers, setFetchedUsers] = React.useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          fetch(`https://api.github.com/users/${userInput}`)
+            .then((result) => {
+              return result.json();
+            })
+            .then((json) => {
+              setFetchedUsers([json].concat(fetchedUsers));
+            });
+        }}
+      >
+        <input
+          placeholder="Type in GitHub username"
+          value={userInput}
+          onChange={(e) => {
+            setUserInput(e.target.value);
+          }}
+        ></input>
+        <button type="submit">Find User</button>
+      </form>
+
+      {fetchedUsers.length > 0 ? (
+        <ul>
+          {fetchedUsers.map((fetchedUser) => (
+            <UserInfo fetchedUser={fetchedUser} />
+          ))}
+        </ul>
+      ) : null}
     </div>
   );
 }
+
+const UserInfo = (props) => {
+  return (
+    <li className="userInfo">
+      <h4>
+        {props.fetchedUser.name
+          ? props.fetchedUser.name
+          : props.fetchedUser.login}
+      </h4>
+      <a
+        href={props.fetchedUser.html_url}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {props.fetchedUser.html_url}
+      </a>
+      <p>{props.fetchedUser.company
+          ? props.fetchedUser.company
+          : "[Unemployed]"}</p>
+      <p>{props.fetchedUser.location
+          ? props.fetchedUser.location
+          : "Alien"}</p>
+      
+    </li>
+  );
+};
 
 export default App;
